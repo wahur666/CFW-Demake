f77a1c5e5d1af535e8bb8240a04ea3ffb37ddfbd
wahur666 <imre.secodi@gmail.com>
2022-09-18 10:14:00 +0200

    upgrade dependecies

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,5 @@
+
+
+pub fn initialize_systems(host: bool) {
+    println!("Initializing systems as {} ....", if host {"host"} else {"guest"});
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,11 @@
     windows_subsystem = "windows"
 )]
 
+mod lib;
+
+use tauri::Manager;
+use crate::lib::initialize_systems;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,6 +16,23 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)] {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools()
+            }
+            let mut host = false;
+            match app.get_cli_matches() {
+                Ok(matches) => {
+                    if matches.args.contains_key("host") {
+                        host = matches.args.get("host").unwrap().value.as_bool().unwrap_or(false);
+                    }
+                }
+                Err(_) => {}
+            }
+            initialize_systems(host);
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
